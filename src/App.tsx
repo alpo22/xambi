3b4cf4560d2e022eda15aa5c27c6c2a860a02163
alpo22 <alan.s.powell@gmail.com>
2024-11-21 12:11:04 -0500

    Export App for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { EditForm, EditEntryType, ValidationType } from "./EditForm";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <EditForm
